20f34db04a7cbfa2b9edad7524d2c86a7724ebf9
PS Le Stang <ps@lestang.fr>
2013-10-07 23:38:57 +0200

    Set version to 0.2.1

--- OCAPy/__init__.py
+++ OCAPy/__init__.py
@@ -3,10 +3,7 @@
 
 __author__ = 'Pierre-Samuel Le Stang'
 __email__ = 'ps@lestang.fr'
-__version__ = '0.2.0'
-
-from OCAPy import OCAPy
-from schema import Schemas, Schema
+__version__ = '0.2.1'
 
 from OCAPy import OCAPy
 from schema import Schemas, Schema
